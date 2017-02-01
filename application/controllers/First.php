a8f449bf878a9f415905acb14ab3c956b8e5abe8
Steven Tsao <steven.tsao777@gmail.com>
2017-02-01 15:04:06 -0800

    closes #3

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -13,6 +13,7 @@
  */
 class First extends Application {
 
+    // #1
     public function index()
     {
 	$record = $this->quotes->get('1');
@@ -21,4 +22,12 @@ class First extends Application {
         $this->render();
     }
     
+    // #3
+    public function zzz() 
+    {
+        $record = $this->quotes->get('1');
+        $this->data = array_merge($this->data, $record);
+        $this->data['pagebody'] = 'justone';
+        $this->render();
+    }
 }
