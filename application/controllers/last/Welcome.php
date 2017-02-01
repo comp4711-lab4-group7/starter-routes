d2a42f8d39ab2d2d20eab4db11ff7306f968dde4
Steven Tsao <steven.tsao777@gmail.com>
2017-02-01 14:52:53 -0800

    closes #2

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,13 @@
+<?php
+
+class Welcome extends Application {
+    
+    // #2
+    public function index()
+    {
+	$record = $this->quotes->get('6');
+        $this->data = array_merge($this->data, $record);
+        $this->data['pagebody'] = 'justone';
+        $this->render();
+    }
+}
