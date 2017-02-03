62a6bc4d65141a26d2312113cd88f642762aa7a6
Chantal09 <carmenchantal@hotmail.com>
2017-02-02 18:33:20 -0800

    issue #10 closed

--- application/controllers/last/Hogwarts.php
+++ application/controllers/last/Hogwarts.php
@@ -6,9 +6,9 @@ class Hogwarts extends Application {
     public function index()
     {
 	      $record = $this->quotes->get('6');
-        $this->data = array_merge($this->data, $record);
-        $this->data['pagebody'] = 'justone';
-        $this->render();
+            $this->data = array_merge($this->data, $record);
+            $this->data['pagebody'] = 'justone';
+            $this->render();
     }
 }
 
